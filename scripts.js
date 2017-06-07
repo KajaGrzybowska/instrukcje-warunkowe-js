ecdbfcc5d44a242b22990a9c7f0e8d7ccd2004b1
KajaGrzybowska <kajagrzybowska@outlook.com>
2017-06-07 17:21:32 +0200

    zadanie + pytanie

---
+++ scripts.js
@@ -0,0 +1,36 @@
+//scripts.js
+/*
+var a = prompt('Wpisz wartość A');
+var	b = prompt('Wpisz wartość B');
+var	value = alert((a * a) + (2 * a * b) - (b * b));
+
+Pytanie: Dlaczego to poniżej w ogóle nie działa?
+
+if (value > 0) {
+	alert('wynik jest dodatni');
+}
+
+
+else if (value < 0) {
+	alert('wynik jest ujemny');
+}
+
+else 
+	alert('wynik jest rowny zero');
+*/
+
+var a = 0;
+var b = 0;
+var value = (a * a) + (2 * a * b) - (b * b);
+
+if (value > 0) {
+	alert('wynik dodani')
+}
+
+else if (value < 0) {
+	alert('wynik ujemny')
+}
+
+else 
+	alert('wynik jest równy 0')
+
